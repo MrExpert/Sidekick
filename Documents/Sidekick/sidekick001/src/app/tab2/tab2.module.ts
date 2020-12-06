14f05a2392bd4e2156d38570641e8f51b42f5e57
Sam U <Sam.Umirdinov@gmail.com>
2020-12-05 18:36:08 -0800

    adds onboarding page and contacts page

--- Documents/Sidekick/sidekick001/src/app/tab2/tab2.module.ts
+++ Documents/Sidekick/sidekick001/src/app/tab2/tab2.module.ts
@@ -5,7 +5,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { AvatarListComponent } from '../avatar-list/avatar-list.component';
+import { ImportContactsComponent } from '../import-contacts/import-contacts.component';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
@@ -16,6 +17,6 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     ExploreContainerComponentModule,
     Tab2PageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page, AvatarListComponent, ImportContactsComponent]
 })
 export class Tab2PageModule {}
